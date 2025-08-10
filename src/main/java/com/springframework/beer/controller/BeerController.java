eff0e0d52b82d1dd6301a8d7d44a8f4ba5691e77
Hasancan Sahan <yamahasirossi@gmail.com>
2025-08-10 18:15:12 +0300

    @Controller changed to @RestController and added listBeers method

--- src/main/java/com/springframework/beer/controller/BeerController.java
+++ src/main/java/com/springframework/beer/controller/BeerController.java
@@ -5,15 +5,22 @@ import com.springframework.beer.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
     private final BeerService beerService;
 
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers(){
+        return beerService.listBeers();
+    }
     public Beer getBeerById(UUID uuid) {
         log.debug("BeerController::getBeerById");
         return beerService.getBeerById(uuid);
