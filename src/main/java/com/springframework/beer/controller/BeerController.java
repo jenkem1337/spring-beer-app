c20be5181292b5b06c52a5d2df62b5dfb79567f3
Hasancan Sahan <yamahasirossi@gmail.com>
2025-08-10 18:20:49 +0300

    new codes added

--- src/main/java/com/springframework/beer/controller/BeerController.java
+++ src/main/java/com/springframework/beer/controller/BeerController.java
@@ -5,7 +5,9 @@ import com.springframework.beer.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,15 +16,20 @@ import java.util.UUID;
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("/api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
+        log.debug("BeerController::listBeers");
         return beerService.listBeers();
     }
-    public Beer getBeerById(UUID uuid) {
+    @RequestMapping(value = "{beerId}", method = RequestMethod.GET)
+    public Beer getBeerById(
+            @PathVariable("beerId")
+            UUID beerId) {
         log.debug("BeerController::getBeerById");
-        return beerService.getBeerById(uuid);
+        return beerService.getBeerById(beerId);
     }
 }
