d1d659031355644900a5c6ade4fa67ee69e1dee9
Hasancan Sahan <yamahasirossi@gmail.com>
2025-08-10 18:15:34 +0300

    added listBeers method to BeerService interface

--- src/main/java/com/springframework/beer/services/BeerService.java
+++ src/main/java/com/springframework/beer/services/BeerService.java
@@ -2,8 +2,10 @@ package com.springframework.beer.services;
 
 import com.springframework.beer.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
     Beer getBeerById(UUID id);
+    List<Beer> listBeers();
 }
