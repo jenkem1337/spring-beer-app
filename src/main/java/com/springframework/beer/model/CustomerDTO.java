408be10a3f06e9a726b0f51cfd809bbe922cec8b
Hasancan Sahan <yamahasirossi@gmail.com>
2025-08-15 16:07:38 +0300

    initial commit

---
+++ src/main/java/com/springframework/beer/model/CustomerDTO.java
@@ -0,0 +1,16 @@
+package com.springframework.beer.model;
+
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Builder
+@Data
+public class CustomerDTO {
+    private UUID id;
+    private String customerName;
+    private Integer version;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+}
