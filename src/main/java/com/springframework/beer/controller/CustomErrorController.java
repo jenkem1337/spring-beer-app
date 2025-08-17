e1cbe957772abf63636c9b9a90e336e5e3a9d732
Hasancan Sahan <yamahasirossi@gmail.com>
2025-08-17 12:58:06 +0300

    initial commit

---
+++ src/main/java/com/springframework/beer/controller/CustomErrorController.java
@@ -0,0 +1,35 @@
+package com.springframework.beer.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.TransactionSystemException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+@ControllerAdvice
+public class CustomErrorController {
+//    @ExceptionHandler
+//    ResponseEntity<Void> handleJPAViolations(TransactionSystemException exception){
+//        System.out.println("exception handler");
+//        return ResponseEntity.badRequest().build();
+//    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<List<Map<String,String>>> handleBindErrors(MethodArgumentNotValidException exception){
+
+        List<Map<String, String>> errorList = exception.getFieldErrors().stream()
+                .map(fieldError -> {
+                    Map<String, String > errorMap = new HashMap<>();
+                    errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+                    return errorMap;
+                }).collect(Collectors.toList());
+
+        return ResponseEntity.badRequest().body(errorList);
+    }
+
+}
