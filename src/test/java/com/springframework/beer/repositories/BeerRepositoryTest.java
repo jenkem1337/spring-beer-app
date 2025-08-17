ef8c759ab4e2d6a871595be29d5145ff557ecd7f
Hasancan Sahan <yamahasirossi@gmail.com>
2025-08-17 13:01:17 +0300

    saveNewBeerNameTooLong test case added and some refactoring

--- src/test/java/com/springframework/beer/repositories/BeerRepositoryTest.java
+++ src/test/java/com/springframework/beer/repositories/BeerRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.springframework.beer.repositories;
 
 import com.springframework.beer.entites.Beer;
+import com.springframework.beer.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,11 +17,30 @@ class BeerRepositoryTest {
     BeerRepository beerRepository;
 
     @Test
+    void saveNewBeerNameTooLong(){
+        assertThrows(ConstraintViolationException.class, () -> {
+            var savedBeer = beerRepository.save(
+                    Beer.builder().beerName("Efes Pilsen 1111111111111111111111111111111111111111111111111111111111111111111111")
+                            .beerStyle(BeerStyle.PILSNER)
+                            .upc("12312312313211")
+                            .price(new BigDecimal("80.00"))
+                            .build()
+            );
+            beerRepository.flush();
+
+        });
+
+    }
+    @Test
     void saveNewBeer() {
         var savedBeer = beerRepository.save(
-                Beer.builder().beerName("Efes Pilsen").build()
+                Beer.builder().beerName("Efes Pilsen")
+                        .beerStyle(BeerStyle.PILSNER)
+                        .upc("12312312313211")
+                        .price(new BigDecimal("80.00"))
+                        .build()
         );
-
+        beerRepository.flush();
         assertThat(savedBeer.getId()).isNotNull();
         assertThat(savedBeer.getBeerName()).isEqualTo("Efes Pilsen");
     }
