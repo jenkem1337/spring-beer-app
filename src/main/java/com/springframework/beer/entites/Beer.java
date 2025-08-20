b790b15f12f080cd4154b7372a8a4ce9ba93d1c9
Hasancan Sahan <yamahasirossi@gmail.com>
2025-08-20 18:13:29 +0300

    creation and update annotations added

--- src/main/java/com/springframework/beer/entites/Beer.java
+++ src/main/java/com/springframework/beer/entites/Beer.java
@@ -6,7 +6,9 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.type.SqlTypes;
 
@@ -49,8 +51,9 @@ public class Beer {
 
     @Version
     private Integer version;
-
+    @CreationTimestamp
     private LocalDateTime createdDate;
+    @UpdateTimestamp
     private LocalDateTime updatedDate;
 
 }
