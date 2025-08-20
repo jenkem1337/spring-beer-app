032639750e119f4623be382107613b24816f7f1e
Hasancan Sahan <yamahasirossi@gmail.com>
2025-08-20 18:14:29 +0300

    dependency added

--- src/test/java/com/springframework/beer/bootstrap/BootstrapDataTest.java
+++ src/test/java/com/springframework/beer/bootstrap/BootstrapDataTest.java
@@ -2,16 +2,20 @@ package com.springframework.beer.bootstrap;
 
 import com.springframework.beer.repositories.BeerRepository;
 import com.springframework.beer.repositories.CustomerRepository;
+import com.springframework.beer.services.BeerCSVService;
+import com.springframework.beer.services.BeerCSVServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@Import(BeerCSVServiceImpl.class)
 class BootstrapDataTest {
     @Autowired
     BeerRepository beerRepository;
@@ -19,18 +23,21 @@ class BootstrapDataTest {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    BeerCSVService beerCSVService;
+
     BootstrapData bootstrapData;
 
     @BeforeEach
     void setUp() {
-        bootstrapData = new BootstrapData(beerRepository, customerRepository);
+        bootstrapData = new BootstrapData(beerRepository, customerRepository, beerCSVService);
     }
 
     @Test
     void testRun() throws Exception {
         bootstrapData.run(null);
 
-        assertThat(beerRepository.count()).isEqualTo(3);
+        assertThat(beerRepository.count()).isEqualTo(2413);
         assertThat(customerRepository.count()).isEqualTo(3);
     }
 
