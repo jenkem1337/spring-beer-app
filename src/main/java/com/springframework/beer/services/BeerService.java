3c09ddd18b21cf790023d42d0ca22e0e0c6d4749
Hasancan Sahan <yamahasirossi@gmail.com>
2025-08-22 14:23:08 +0300

    listBeers method signature changed

--- src/main/java/com/springframework/beer/services/BeerService.java
+++ src/main/java/com/springframework/beer/services/BeerService.java
@@ -1,6 +1,7 @@
 package com.springframework.beer.services;
 
 import com.springframework.beer.model.BeerDTO;
+import com.springframework.beer.model.BeerStyle;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,7 +9,7 @@ import java.util.UUID;
 
 public interface BeerService {
     Optional<BeerDTO> getBeerById(UUID id);
-    List<BeerDTO> listBeers(String beerName);
+    List<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory);
     BeerDTO saveNewBeer(BeerDTO beerDTO);
 
     Optional<BeerDTO> updateBeerById(UUID beerId, BeerDTO beerDTO);
